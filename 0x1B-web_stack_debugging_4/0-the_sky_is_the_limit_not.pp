6d927d7569151fd0de4b2c8034aa9f0a293efcb5
Esther Ndosi <estherndosi24@gmail.com>
2024-09-04 06:29:49 -0700

    Fix limit

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,11 +1,4 @@
 # Fix webstack to serve more requests
 
-exec { 'increase_limit':
-  command => 'sudo sed -i "s/15/1000/" /etc/default/nginx',
-  path    => '/usr/local/bin/:/bin/'
-}
-
--> exce { 'restart':
-  command => 'sudo service nginx restart'
-  path    => '/etc/init.d/'
-}
+exec { '/usr/bin/env sed -i s/15/1000/ /etc/default/nginx': }
+-> exec { '/usr/bin/env service nginx restart': }
