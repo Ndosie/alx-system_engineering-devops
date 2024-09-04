acbf58877ccddaee66137a055dcdd393d634b773
Esther Ndosi <estherndosi24@gmail.com>
2024-09-04 04:49:03 -0700

    Strace is your friend

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,6 @@
 # find why apache is returning 500 error and fix the issue
 
 exec { 'Fix wordpress settings file':
-	command => "sed -i 's/.phpp/.php/' /var/www/html/wp-settings.php"
-	path => '/bin'
+  command => "sed -i 's/.phpp/.php/' /var/www/html/wp-settings.php"
+  path => '/bin'
 }
